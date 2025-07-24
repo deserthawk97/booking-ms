5cf9d0eb705df54161a42efc3e24f7cc9b569fb9
sushankumar <sushankumar97@gmail.com>
2025-07-24 21:32:27 +0530

    70%

--- src/main/java/com/example/booking_ms/hotel.java
+++ src/main/java/com/example/booking_ms/hotel.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class hotel {
     @GetMapping("/hotel")
     public String getData() {
-        return "Please  book hotels ticket with 60% discount";
+        return "Please  book hotels ticket with 70% discount";
     }
 }
